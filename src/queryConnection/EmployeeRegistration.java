b7c1f431255fa6f477bc9ba4c736ee0998a4e5e8
edobbin <ethandobbin37@gmail.com>
2023-12-06 20:17:11 -0500

    progress

--- src/queryConnection/EmployeeRegistration.java
+++ src/queryConnection/EmployeeRegistration.java
@@ -68,8 +68,9 @@ class EmployeeRegistrationFrame extends JFrame {
             	String Name = nameField.getText();
             	String StaffType = staffTypeComboBox.getItemAt(2);
             	String Salary = salaryField.getText();
+                String phoneNumber = phoneNumberField.getText();
                 
-                addEmployeeToDatabase(EmployeeID, SSN, HoursWorked, Name, StaffType, Salary);
+                addEmployeeToDatabase(EmployeeID, SSN, HoursWorked, Name, StaffType, Salary, phoneNumber);
 
             }
         });
@@ -79,7 +80,7 @@ class EmployeeRegistrationFrame extends JFrame {
     private void generateUniqueEmployeeId() {
         Random random = new Random();
         while (true) {
-            String employeeId = String.format("%09d", random.nextInt(1000000000));
+            String employeeId = String.format("%09d", random.nextInt(999999999));
             if (!employeeIdSet.contains(employeeId)) {
                 employeeIdSet.add(employeeId);
                 employeeIdLabel.setText(employeeId);
@@ -87,23 +88,43 @@ class EmployeeRegistrationFrame extends JFrame {
             }
         }
     }
-    public void addEmployeeToDatabase(String empID,String SSN, String hours, String name, String type,String salary) {
+    public void addEmployeeToDatabase(String empID,String SSN, String hours, String name, String type,String salary,String phone) {
             // JDBC database connection parameters
             final String username = "eziegl4";
         	final String password = "COSC*26yaj";
         	final String url = "jdbc:mysql://triton.towson.edu:3360/?serverTimezoneEST#/"+username+"db";
-
+            empID = "2";
             try (Connection connection = DriverManager.getConnection(url, username, password)) {
-                String sql = "DELETE FROM eziegl4db.Worker WHERE employee_id = ?;\n INSERT INTO eziegl4db.Worker (EmployeeID, SSN, Name, StaffType, Salary, PhoneNumbe) VALUES ("+empID;
-                
+                String sql = "INSERT INTO EarningsPayment (TID, TotalCost, Tip, ServerID) VALUES (?, ?, ?,?);INSERT INTO Worker (EmployeeID, SSN, HoursWorked, Name, StaffType, Salary, PhoneNumber) VALUES (?,?,?,?,?,?,?);";
+                String temp1 =""+(Math.floor(Math.random() * (100 - 10) + 10) / 100), temp2=""+(Math.floor(Math.random() * (30 - 1) + 1) / 100);
                 try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
                     preparedStatement.setString(1, empID);
+                    preparedStatement.setString(2, temp1);
+                    preparedStatement.setString(3, temp2);
+                    preparedStatement.setString(4, empID);
+
+                    preparedStatement.setString(5, empID);
+                    preparedStatement.setString(6, SSN);
+                    preparedStatement.setString(7, hours);
+                    preparedStatement.setString(8, name);
+                    preparedStatement.setString(9, type);
+                    preparedStatement.setString(10, salary);
+                    preparedStatement.setString(11, phone);
+
+                
+                JOptionPane.showMessageDialog(EmployeeRegistrationFrame.this, "Employee Registered!");
+                this.dispose();
                 }
 
-                JOptionPane.showMessageDialog(EmployeeRegistrationFrame.this, "Employee Registered!");
+                
             } catch (SQLException ex) {
+                
                 ex.printStackTrace();
                 JOptionPane.showMessageDialog(this, "Error connecting to the database");
+            
+            System.out.println(ex);
+            System.out.println(empID);
+            
             }
         }
 }
